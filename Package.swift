f8e5275f70c56e0fb9c022f413072b5f099c7273
Build Er <builder@macmini6.local>
2023-03-15 09:09:23 -0300

    Package version: 1.0.0-beta+20230315120127

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXFoundationWrapper",
 				dependencies: [
 					"GXFoundation",
-					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.tvOS, .watchOS, .iOS]))
+					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFoundation",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXFoundation-1.0.0-beta+20230314121632.xcframework.zip",
-			checksum: "fe76f43affe1b9e093470157f9100236c68eef1a5d35ab30c9c0fc9d9369c941"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFoundation-1.0.0-beta+20230315120127.xcframework.zip",
+			checksum: "4571f34e0231b196eb7f0c0966e4b93e1f4a69b8597c6cbf456b99140ad69a34"
 		)
 	]
 )
