df21c4b27042196ceeec72660050038ddf478d93
genexusbot <builder@genexus.com>
2023-07-29 18:41:20 -0300

    Package version: 1.1.0-rc.1

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXFoundation",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXFoundation",
+			targets: ["GXFoundationWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/YAJL.git", exact: "1.1.0-rc.1")
+	],
+	targets: [
+		.target(name: "GXFoundationWrapper",
+				dependencies: [
+					"GXFoundation",
+					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXFoundation",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXFoundation-1.1.0-rc.1.xcframework.zip",
+			checksum: "ce1703c41426023d9fabf6eabd168881390a6302cd2f3289d173f2fd3bef3cc4"
+		)
+	]
+)
