db794a7e3cd30138fa1db6f4684cec9ac1f9559e
genexusbot <builder@genexus.com>
2024-02-09 15:31:49 -0300

    Package version: 1.4.26

--- Package.swift
+++ Package.swift
@@ -3,26 +3,26 @@ import PackageDescription
 
 let package = Package(
 	name: "GXFoundation",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0")],
 	products: [
 		.library(
 			name: "GXFoundation",
 			targets: ["GXFoundationWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/YAJL.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/YAJL.git", exact: "1.4.26")
 	],
 	targets: [
 		.target(name: "GXFoundationWrapper",
 				dependencies: [
 					"GXFoundation",
-					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
+					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFoundation",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXFoundation-1.1.0.xcframework.zip",
-			checksum: "e3d413be212613f4c3b49db955df9afe2d1d1fceb6c767db93b1bb0d731ad032"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXFoundation-1.4.26.xcframework.zip",
+			checksum: "0444f998f20dcc8a64af91a693be6b693d19037af14ee7ad05f6b7af0a2bd83e"
 		)
 	]
 )
