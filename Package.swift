f7fed69d9c7d58eb46c8d007dbf695300d2b59f2
genexusbot <builder@genexus.com>
2024-02-19 14:54:14 -0300

    Package version: 1.5.0-beta.4

--- Package.swift
+++ Package.swift
@@ -3,26 +3,26 @@ import PackageDescription
 
 let package = Package(
 	name: "GXFoundation",
-	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0"), .visionOS("1.0")],
 	products: [
 		.library(
 			name: "GXFoundation",
 			targets: ["GXFoundationWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/YAJL.git", exact: "1.5.0-beta.3")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/YAJL.git", exact: "1.5.0-beta.4")
 	],
 	targets: [
 		.target(name: "GXFoundationWrapper",
 				dependencies: [
 					"GXFoundation",
-					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
+					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.iOS, .watchOS, .tvOS, .visionOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFoundation",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXFoundation-1.5.0-beta.3.xcframework.zip",
-			checksum: "a5693e4efdcb475672579af9943145d9139a867e4a13c0dddac87c28c9e68913"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXFoundation-1.5.0-beta.4.xcframework.zip",
+			checksum: "9ed0e76356cb4878f752522296144eecea2397299190f66be9470bd33a91f668"
 		)
 	]
 )
