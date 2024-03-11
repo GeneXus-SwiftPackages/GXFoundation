6bcaaa1c4677364f6e7ae4bbf7bf6d886eabce4f
genexusbot <builder@genexus.com>
2024-03-11 12:23:59 -0300

    Package version: 1.3.5

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXFoundationWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/YAJL.git", exact: "1.3.3")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/YAJL.git", exact: "1.3.5")
 	],
 	targets: [
 		.target(name: "GXFoundationWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXFoundation",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXFoundation-1.3.3.xcframework.zip",
-			checksum: "b34f32db12a6915a1c4f05cfb8db5cfac1764d8c81e40fd216f5d1d48ca6bb94"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXFoundation-1.3.5.xcframework.zip",
+			checksum: "d7d3cbe700f4447398d61281c7b22b26df3185fb4a74b8ae20ba3db530afe504"
 		)
 	]
 )
